c19327e08ec2ed2f4133cdd77c64453f7d54022c
Thomasjkeel <tomkee@ceh.ac.uk>
2026-04-17 13:44:43 +0100

    add files

---
+++ src/rainfall_gridder/prepare_data/data_formatting.py
@@ -0,0 +1,30 @@
+import duckdb
+
+# NEGATIVE VALUES SET TO NULL
+data_w_no_nulls = gauge_data.with_columns(
+    pl.when(pl.col(DATA_PRECIPITATION_COL) < 0)
+    .then(None)
+    .otherwise(pl.col(DATA_PRECIPITATION_COL))
+    .alias(DATA_PRECIPITATION_COL)
+
+
+## NOT USED
+metadata_only_duplicated = (
+    all_metadata
+    .with_columns(
+        pl.len().over(["EASTING", "NORTHING"]).alias("count")
+    )
+    .filter(pl.col("count") > 1)
+)
+
+
+metadata_w_groupIDs = all_metadata.with_columns(
+    pl.struct("EASTING", "NORTHING")
+    .rank(method="dense")
+    .alias("station_group_id")
+)
+
+
+metadata_w_paths = metadata_w_groupIDs.with_columns(
+    pl.lit(None).cast(pl.String).alias("file_path")
+)
