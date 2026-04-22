01ad81337f761fe8e59de290f97710c23af27624
Thomasjkeel <tomkee@ceh.ac.uk>
2026-04-22 12:04:55 +0100

    add outline of datapreparer

--- src/rainfall_gridder/prepare_data/DataPreparer.py
+++ src/rainfall_gridder/prepare_data/DataPreparer.py
@@ -1,6 +1,6 @@
 import polars as pl
 import xarray as xr
-from rainfall_gridder import prepare_data
+from rainfall_gridder.prepare_data import data_formatting, metadata_preparer
 from rainfall_gridder.utils import xarray_utils
 
 
@@ -10,16 +10,23 @@ class DataPreparer:
         data: pl.DataFrame,
         metadata: pl.DataFrame,
         station_id_col: str,
+        precipitation_col: str,
         date_time_col: str,
+        easting_col: str,
+        northing_col: str,
         gridded_data: xr.Dataset,
     ):
         self.data = data
         self.station_id_col = station_id_col
+        self.precipitation_col = precipitation_col
         self.date_time_col = date_time_col
+        self.easting_col = easting_col
+        self.northing_col = northing_col
         self.metadata = self._prepare_metadata(metadata)
         self.gridded_data = gridded_data
 
     def _remove_duplicates_in_metadata(self, metadata):
+        
         return metadata.unique(
             subset=[self.station_id_col]
         )  # TODO: this would leave wrong coords if it returns first unique
@@ -27,16 +34,22 @@ class DataPreparer:
     def _prepare_metadata(self, metadata):
         metadata = self._remove_duplicates_in_metadata(metadata)
         try:
-            prepare_data.metadata_preparer.add_completeness_to_metadata(
+            metadata_preparer.add_completeness_to_metadata(
                 self.data, metadata, station_id_col=self.station_id_col, date_time_col=self.date_time_col
             )
         except ValueError as ve:
             print(ve)
 
-        return prepare_data.metadata_preparer.add_completeness_to_metadata(
+        metadata = metadata_preparer.add_completeness_to_metadata(
             self.data, metadata, station_id_col=self.station_id_col, date_time_col=self.date_time_col
         )
+        metadata = data_formatting.group_metadata_by_station_locations(metadata, easting_col=self.easting_col, northing_col=self.northing_col)
+        return data_formatting.add_blank_file_path_to_metadata(metadata)
 
     def _prepare_gridded_data(self, gridded_data):
         gridded_data = xarray_utils.replace_daily_time_step_hour_with_zero(gridded_data, self.date_time_col)
-        return self
+        gridded_data = xarray_utils.subset_gridded_data_to_metadata_bounds(gridded_data, self.metadata, self.easting_col, self.northing_col)
+        return gridded_data
+    
+    def _prepare_data(self):
+        return data_formatting.set_negative_precip_values_to_none(self.data, precip_col=self.precipitation_col)
--- src/rainfall_gridder/utils/xarray_utils.py
+++ src/rainfall_gridder/utils/xarray_utils.py
@@ -1,3 +1,4 @@
+import polars as pl
 import xarray as xr
 
 
@@ -22,3 +23,29 @@ def replace_daily_time_step_hour_with_zero(daily_data: xr.Dataset, date_time_col
     """
     replacement_time = daily_data[date_time_col].to_index().map(lambda t: t.replace(hour=0))
     return daily_data.assign_coords(time=replacement_time)
+
+
+def subset_gridded_data_to_metadata_bounds(gridded_data: xr.Dataset, metadata: pl.DataFrame, easting_col: str, northing_col: str) -> xr.Dataset:
+    """
+    Subset xarray data to bounds of metadata (adds a small buffer of size standard deviation).
+
+    Parameters
+    ----------
+    gridded_data:
+        Gridded data to subset
+    metadata:
+        Metadata with easting and northing column
+    easting_col:
+        Name of east/west column
+    northing_col:
+        Name of north/south column
+
+    Returns
+    -------
+    subset_gridded_data:
+        Gridded data subset to easting and northing bounds of metadata
+    """
+    return gridded_data.sel(x=slice(metadata[easting_col].min() - metadata[easting_col].std(),
+                   metadata[easting_col].max() + metadata[easting_col].std()),
+            y=slice(metadata[northing_col].min() - metadata[northing_col].std(),
+                   metadata[northing_col].max() + metadata[northing_col].std()))
