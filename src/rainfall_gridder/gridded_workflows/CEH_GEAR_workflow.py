4329d61b872f2e6df4e9e2538b4f6d0292835580
Thomasjkeel <tomkee@ceh.ac.uk>
2026-04-22 09:36:34 +0100

    add psuedocode for batch saving and producing

--- src/rainfall_gridder/gridded_workflows/CEH_GEAR_workflow.py
+++ src/rainfall_gridder/gridded_workflows/CEH_GEAR_workflow.py
@@ -5,6 +5,7 @@ from rainfall_gridder.generate_grids import ceh_gear_subdaily_producer
 from rainfall_gridder.config.configs import get_ceh_gear_defaults
 from rainfall_gridder.config.schema import WorkflowConfig, ColumnConfig
 
+from rainfall_gridder.utils import batch_saving_utils
 
 def ceh_gear_subdaily_workflow(
     data_path: str, metadata_path: str, columns: dict | ColumnConfig | None = None, **overrides
@@ -35,7 +36,26 @@ def ceh_gear_subdaily_workflow(
     data, metadata = apply_intenseQC_rulebase(data, metadata, config.output_dir)
 
     # 3. Generate grids
-    data, metadata = ceh_gear_subdaily_producer(data, metadata, config.output_dir)
+    all_days = batch_saving_utils.get_all_days(metadata, start_date_col=config.start_date_col, end_date_col=config.end_date_col)
+    
+    for batch_days in batch_saving_utils.batch_days(all_days, config.batch_size):
+        # batch_results = []
+        for time_step in batch_days:
+            # one_day_gridded_daily = gridded_daily.sel(time=time_step.replace(minute=0, second=0, microsecond=0)).where(uk_mask) # subset_to_uk_mask to work with map multiplication
+            # ceh_gear_sub_daily_producer = CEHGEARSubDailyProducer(rainfall_data, metadata, time_step, data_resolution=TIME_RES,
+            #                                                 rain_gauge_col=PRECIPITATION_COL, station_id_col=STATION_ID_COL,
+            #                                                 easting_col=EASTING_COL, northing_col=NORTHING_COL, date_col=DATE_TIME_COL,
+            #                                                 hour_at_start_of_day=RAINFALL_OFFSET_HOURS)
+            # data, metadata = ceh_gear_subdaily_producer(data, metadata, config.output_dir, one_day_gridded)
+            pass
+        # combined_batch_ds = xr.concat(batch_results, dim=config.datetime_col)
+        # batch_saving_utils.write_to_zarr(config.output_zarr_file, zarr_format=2)
+        pass
+
+
+
 
     # 4. Save outputs
     return
+
+
