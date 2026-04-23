4f2db8388a762ac945b33ceaacf6d3125dc04188
Thomasjkeel <tomkee@ceh.ac.uk>
2026-04-23 17:37:26 +0100

    rename to rainfall_data and rainfall_metadata

--- src/rainfall_gridder/prepare_data/DataPreparer.py
+++ src/rainfall_gridder/prepare_data/DataPreparer.py
@@ -13,8 +13,8 @@ class DataPreparer:
     """
     def __init__(
         self,
-        data: pl.DataFrame,
-        metadata: pl.DataFrame,
+        rainfall_data: pl.DataFrame,
+        rainfall_metadata: pl.DataFrame,
         station_id_col: str,
         station_name_col: str,
         precipitation_col: str,
@@ -35,6 +35,10 @@ class DataPreparer:
 
         Parameters
         ----------
+        rainfall_data:
+            Rainfall gauge data
+        rainfall_metadata:
+            Details of rain gauge data
         gridded_rainfall_col:
             Name of rainfall variable in the gridded_rainfall_data
         rainfall_offset_hours:
@@ -58,8 +62,8 @@ class DataPreparer:
         self.gridded_rainfall_col = gridded_rainfall_col
 
         # Prepare data inputs
-        self.data = self._prepare_data(data)
-        self.metadata = self._prepare_metadata(metadata)
+        self.rainfall_data = self._prepare_data(rainfall_data)
+        self.rainfall_metadata = self._prepare_metadata(rainfall_metadata)
         self.gridded_rainfall_data = self._prepare_gridded_rainfall_data(gridded_rainfall_data)
 
         # empty final outputs
@@ -71,22 +75,22 @@ class DataPreparer:
             subset=[self.station_id_col]
         )  # TODO: this would leave wrong coords if it returns first unique
 
-    def _prepare_metadata(self, metadata: pl.DataFrame) -> pl.DataFrame:
-        metadata = self._remove_duplicates_in_metadata(metadata)
+    def _prepare_metadata(self, rainfall_metadata: pl.DataFrame) -> pl.DataFrame:
+        rainfall_metadata = self._remove_duplicates_in_metadata(rainfall_metadata)
         try:
             metadata_preparer.add_completeness_to_metadata(
-                self.data, metadata, station_id_col=self.station_id_col, date_time_col=self.date_time_col
+                self.rainfall_data, rainfall_metadata, station_id_col=self.station_id_col, date_time_col=self.date_time_col
             )
         except ValueError as ve:
             print(ve)
 
-        metadata = metadata_preparer.add_completeness_to_metadata(
-            self.data, metadata, station_id_col=self.station_id_col, date_time_col=self.date_time_col
+        rainfall_metadata = metadata_preparer.add_completeness_to_metadata(
+            self.rainfall_data, rainfall_metadata, station_id_col=self.station_id_col, date_time_col=self.date_time_col
         )
-        metadata = data_formatting.group_metadata_by_station_locations(
-            metadata, easting_col=self.easting_col, northing_col=self.northing_col
+        rainfall_metadata = data_formatting.group_metadata_by_station_locations(
+            rainfall_metadata, easting_col=self.easting_col, northing_col=self.northing_col
         )
-        return data_formatting.add_blank_file_path_to_metadata(metadata)
+        return data_formatting.add_blank_file_path_to_metadata(rainfall_metadata)
 
     def _prepare_gridded_rainfall_data(self, gridded_rainfall_data: xr.Dataset) -> xr.Dataset:
         for data_var in ["x", "y", "time", self.gridded_rainfall_col]:
@@ -98,7 +102,7 @@ class DataPreparer:
             gridded_rainfall_data, time_col="time"
         )
         gridded_rainfall_data = xarray_utils.subset_gridded_data_to_metadata_bounds(
-            gridded_rainfall_data, self.metadata, self.easting_col, self.northing_col
+            gridded_rainfall_data, self.rainfall_metadata, self.easting_col, self.northing_col
         )
         return gridded_rainfall_data
 
@@ -149,9 +153,9 @@ class DataPreparer:
         prepared_metadata_list = []
 
         # Loop through each station id group (group may be multiple gauges if a site has a backup)
-        for station_group_id in self.metadata["station_group_id"].unique():
-            metadata_one_group = self.metadata.filter(pl.col("station_group_id") == station_group_id)
-            data_one_group = self.data.filter(
+        for station_group_id in self.rainfall_metadata["station_group_id"].unique():
+            metadata_one_group = self.rainfall_metadata.filter(pl.col("station_group_id") == station_group_id)
+            data_one_group = self.rainfall_data.filter(
                 pl.col(self.station_id_col).is_in(metadata_one_group[self.station_id_col].unique().to_list())
             )
 
@@ -228,9 +232,9 @@ class DataPreparer:
                     min_datetime=data_one_group[self.date_time_col].min(),
                     max_datetime=data_one_group[self.date_time_col].max(),
                 )
-                prepared_metadata_list.append(merged_metadata.select(self.metadata.columns))
+                prepared_metadata_list.append(merged_metadata.select(self.rainfall_metadata.columns))
             else:
-                prepared_metadata_list.append(metadata_one_group.select(self.metadata.columns))
+                prepared_metadata_list.append(metadata_one_group.select(self.rainfall_metadata.columns))
         self.prepared_data = pl.concat(prepared_data_list)
         self.prepared_metadata = pl.concat(prepared_metadata_list, how="diagonal_relaxed")
 
