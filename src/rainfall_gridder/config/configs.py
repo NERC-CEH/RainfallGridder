fe8de2bddb7fdb17347980b8ff89ce8a2b664d68
Thomasjkeel <tomkee@ceh.ac.uk>
2026-04-23 09:52:05 +0100

    add rainfall col default

--- src/rainfall_gridder/config/configs.py
+++ src/rainfall_gridder/config/configs.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 def get_ceh_gear_15m_CEH_GEAR_based_kwargs():
     return {
+        "gridded_rainfall_col": "rainfall_amount",
         "output_dir": Path("outputs"),
         "rainfall_offset_hours": 10,
         "n_hours": 96,
@@ -12,8 +13,10 @@ def get_ceh_gear_15m_CEH_GEAR_based_kwargs():
 
 def get_ceh_gear_15m_HadUK_Grid_based_kwargs():
     return {
+        "gridded_rainfall_col": "rainfall",
         "output_dir": Path("outputs"),
         "rainfall_offset_hours": 9,
         "n_hours": 96,
         "min_n_timesteps": 100,  # TODO: change
     }
+
