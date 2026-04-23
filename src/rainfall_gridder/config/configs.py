674561e33b535bde5f1ba2aa9a3f07b24f736415
Thomasjkeel <tomkee@ceh.ac.uk>
2026-04-23 11:54:21 +0100

    edit kwargs

--- src/rainfall_gridder/config/configs.py
+++ src/rainfall_gridder/config/configs.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def get_ceh_gear_15m_CEH_GEAR_based_kwargs():
     return {
-        "output_dir": Path("outputs"),
+        "output_dir": Path("ceh_gear_15m_CEH_GEAR_based"),
         "rainfall_offset_hours": 10,
         "n_hours": 96,
         "min_n_timesteps": 100,  # TODO: change
@@ -12,7 +12,7 @@ def get_ceh_gear_15m_CEH_GEAR_based_kwargs():
 
 def get_ceh_gear_15m_HadUK_Grid_based_kwargs():
     return {
-        "output_dir": Path("outputs"),
+        "output_dir": Path("ceh_gear_15m_HadUK_Grid_based"),
         "rainfall_offset_hours": 9,
         "n_hours": 96,
         "min_n_timesteps": 100,  # TODO: change
