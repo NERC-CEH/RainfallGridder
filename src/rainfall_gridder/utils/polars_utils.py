5f9c0c379641fa5f8db5e290125cb864d8c3a7a9
Thomasjkeel <tomkee@ceh.ac.uk>
2026-04-27 14:36:34 +0100

    ruff format

--- src/rainfall_gridder/utils/polars_utils.py
+++ src/rainfall_gridder/utils/polars_utils.py
@@ -2,12 +2,13 @@ import polars as pl
 
 import rainfallqc.utils.data_utils
 
+
 def check_data_is_specific_time_res(data: pl.DataFrame, time_res: str | list) -> None:
     """
     Check data has a hourly or daily time step.
 
     Taken from RainfallQC.
-    
+
     Does not work for monthly data, please use 'check_data_is_monthly'.
 
     Parameters
