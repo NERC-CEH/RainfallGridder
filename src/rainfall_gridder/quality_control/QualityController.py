15a859734c47ba26fea7ab1599966b4fc8326a87
Thomasjkeel <tomkee@ceh.ac.uk>
2026-04-27 12:34:43 +0100

    remove run

--- src/rainfall_gridder/quality_control/QualityController.py
+++ src/rainfall_gridder/quality_control/QualityController.py
@@ -242,7 +242,7 @@ class QualityController:
                 continue
 
             # Summarise QC flags into statistics
-            qc_summariser = QCSummariser.run(station_id, nearby_metadata, nearby_rainfall_data, qc_result)
+            qc_summariser = QCSummariser(station_id, nearby_metadata, nearby_rainfall_data, qc_result)
 
             # Apply rulebase
             rule_removed_rows, n_rows_removed = apply_intenseQC_rulebase(qc_summariser.all_flags, station_id)
