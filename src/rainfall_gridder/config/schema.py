8b95e09ca4f90871e1a838e6e206da321b97990e
Thomasjkeel <tomkee@ceh.ac.uk>
2026-06-23 15:06:44 +0100

    change to try parse hive

--- src/rainfall_gridder/config/schema.py
+++ src/rainfall_gridder/config/schema.py
@@ -62,7 +62,7 @@ class WorkflowConfig(BaseModel):
         rainfall_data_path = Path(self.rainfall_data.path)
 
         if rainfall_data_path.suffix == ".parquet":
-            return pl.read_parquet(rainfall_data_path, try_parse_dates=True)
+            return pl.read_parquet(rainfall_data_path, try_parse_hive_dates=True)
 
         if rainfall_data_path.suffix == ".csv":
             return pl.read_csv(rainfall_data_path, try_parse_dates=True)
@@ -71,7 +71,7 @@ class WorkflowConfig(BaseModel):
             return pl.scan_parquet(rainfall_data_path, try_parse_hive_dates=True).collect()
         except (ComputeError, InvalidOperationError):
             try:
-                return pl.scan_csv(rainfall_data_path, try_parse_dates=True).collect()
+                return pl.scan_csv(rainfall_data_path, try_parse_hive_dates=True).collect()
             except (ComputeError, InvalidOperationError) as err:
                 raise ValueError(
                     f"Problem with files in rainfall data input path: {path}"
